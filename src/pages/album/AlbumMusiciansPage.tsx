cd712a6e3b3523ddaf9ac80a192eecae1343c132
hwangtab <hwangtab@gmail.com>
2025-12-09 13:06:19 +0900

    feat: reorganize album routes under /album/* with redirects

---
+++ src/pages/album/AlbumMusiciansPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MusiciansSection from '../../components/home/MusiciansSection';
+
+const AlbumMusiciansPage = () => (
+  <div className="min-h-screen bg-light-beige">
+    <MusiciansSection />
+  </div>
+);
+
+export default AlbumMusiciansPage;
---
+++ src/pages/album/AlbumTracksPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TracksSection from '../../components/home/TracksSection';
+
+const AlbumTracksPage = () => (
+  <div className="min-h-screen bg-light-beige">
+    <TracksSection />
+  </div>
+);
+
+export default AlbumTracksPage;
