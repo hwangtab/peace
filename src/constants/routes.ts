54e72942a00d17850af38ced22c215bd6282d276
hwangtab <hwangtab@gmail.com>
2025-12-19 00:39:29 +0900

    refactor: use ROUTES constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,21 @@
+export const ROUTES = {
+    HOME: '/',
+    GALLERY: '/gallery',
+    VIDEOS: '/videos',
+    PRESS: '/press',
+    CAMPS: {
+        ROOT: '/camps',
+        CAMP_2023: '/camps/2023',
+        CAMP_2025: '/camps/2025',
+        CAMP_2026: '/camps/2026',
+    },
+    ALBUM: {
+        ABOUT: '/album/about',
+        MUSICIANS: '/album/musicians',
+        TRACKS: '/album/tracks',
+    },
+    LEGACY: {
+        MUSICIANS: '/musicians',
+        TRACKS: '/tracks',
+    },
+} as const;
