bcbe9f0b96eb714d77ebcd605ea358c8a72e1ae7
hwangtab <hwangtab@gmail.com>
2025-12-19 00:39:29 +0900

    refactor: use ROUTES constants

--- src/components/layout/navigationData.ts
+++ src/components/layout/navigationData.ts
@@ -1,19 +1,21 @@
+import { ROUTES } from '../../constants/routes';
+
 export const simpleMenuItems = [
-    { name: '홈', path: '/' },
-    { name: '갤러리', path: '/gallery' },
-    { name: '비디오', path: '/videos' },
-    { name: '언론보도', path: '/press' },
+    { name: '홈', path: ROUTES.HOME },
+    { name: '갤러리', path: ROUTES.GALLERY },
+    { name: '비디오', path: ROUTES.VIDEOS },
+    { name: '언론보도', path: ROUTES.PRESS },
 ];
 
 export const campItems = [
-    { name: '2023 캠프', path: '/camps/2023' },
-    { name: '2025 캠프', path: '/camps/2025' },
-    { name: '2026 캠프', path: '/camps/2026' },
-    { name: '모든 캠프', path: '/camps' },
+    { name: '2023 캠프', path: ROUTES.CAMPS.CAMP_2023 },
+    { name: '2025 캠프', path: ROUTES.CAMPS.CAMP_2025 },
+    { name: '2026 캠프', path: ROUTES.CAMPS.CAMP_2026 },
+    { name: '모든 캠프', path: ROUTES.CAMPS.ROOT },
 ];
 
 export const albumItems = [
-    { name: '앨범 소개', path: '/album/about' },
-    { name: '뮤지션', path: '/album/musicians' },
-    { name: '수록곡', path: '/album/tracks' },
+    { name: '앨범 소개', path: ROUTES.ALBUM.ABOUT },
+    { name: '뮤지션', path: ROUTES.ALBUM.MUSICIANS },
+    { name: '수록곡', path: ROUTES.ALBUM.TRACKS },
 ];
