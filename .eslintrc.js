0e95ba73c2358cb81fe978c77bd25d26339833b3
hwangtab <hwangtab@gmail.com>
2025-12-20 21:27:25 +0900

    chore: setup Prettier and enhance ESLint with accessibility rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
-  extends: ['react-app'],
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:jsx-a11y/recommended'
+  ],
+  plugins: ['jsx-a11y'],
   env: {
     browser: true,
     node: true,
     es6: true
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'warn'
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react/jsx-no-target-blank': 'error',
+    'jsx-a11y/alt-text': 'error',
+    'jsx-a11y/aria-props': 'error',
+    'jsx-a11y/aria-role': 'error',
+    'jsx-a11y/role-has-required-aria-props': 'error',
+    'jsx-a11y/heading-has-content': 'error'
   }
 };
